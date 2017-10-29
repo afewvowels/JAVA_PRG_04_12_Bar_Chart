d8e6e7ee61d1e28712b0cbbef84a0332459d79ea
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-29 12:22:24 -0400

    Prompt entered. Ready for code to be written.

---
+++ src/java_prg_04_12_bar_chart/JAVA_PRG_04_12_Bar_Chart.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_04_12_bar_chart;
+
+/**
+ *
+ * @author bluebackdev
+ * Write a program that asks the user to enter today's sales for five stores.
+ * The program should display a bar chart comparing each store's sales. Create
+ * each bar in the bar chart by displaying a row of asterisks. Each asterisk
+ * should represent $100 of sales. Here is an example of the program's output.
+ * 
+ *      Enter Today's sales for store 1: 1000 [Enter]
+ *      Enter Today's sales for store 2: 1200 [Enter]
+ *      Enter Today's sales for store 3: 1800 [Enter]
+ *      Enter Today's sales for store 4: 800 [Enter]
+ *      Enter Today's sales for store 5: 1900 [Enter]
+ * 
+ *      SALES BAR CHART
+ *      Store 1: **********
+ *      Store 2: ************
+ *      Store 3: ******************
+ *      Store 4: ********
+ *      Store 5: *******************
+ * 
+ */
+public class JAVA_PRG_04_12_Bar_Chart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
